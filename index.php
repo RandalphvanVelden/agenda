0792757625a0ca3e3a213dd00c489df4ff7eb57b
RandalphvanVelden <45844254+RandalphvanVelden@users.noreply.github.com>
2021-09-24 15:37:24 +0200

    created a few pages

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'connect.php';
+
+?>
+<button class="button log" type="submit" onClick= "parent.location='login.php'">log in</button>
+<button class="button log" type="submit" onClick = "parent.location='newuser.php'"  >nieuwe gebruiker</button>
+<?php
+
+?>
+
+
+
+
+<script>
---
+++ newuser.php
@@ -0,0 +1,14 @@
+<?php
+include 'connect.php';
+?>
+<form action="index.php" method="post">
+Voornaam: <input type="text" name="firstname"><br>
+Achternaam: <input type="text" name="lastname"><br>
+Phone: <input type="tel" name="firstname"><br>
+E-mail: <input type="email" name="email"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+
+
